65d6b9c52e3099be18eac24f1571b76a91147350
Jonathan <phyogaiom@gmail.com>
2023-09-25 20:39:13 +0100

    Vue Project Initial Commit

---
+++ pizzeria/pizzeria-iles/src/site.ts
@@ -0,0 +1,11 @@
+export default {
+  title: `Vue Designer`,
+  description: 'Vue Designer îles Tailwind CSS - Quick start template',
+  author: 'Pinegrow',
+  url: 'https://pg-iles-tailwindcss.netlify.app',
+  nav: [
+    { text: 'Home', link: '/' },
+    { text: `Quick Start`, link: '/quick-start' },
+    { text: 'Subscribe', link: '/subscribe' },
+  ],
+}
